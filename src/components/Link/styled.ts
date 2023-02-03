3e2acb0e6c13f8ba67fbace825a3f621e7381b07
Alessio Ilardi <alessio.ilardi@gmail.com>
2023-02-03 12:40:52 +0100

    📦 NEW: add Link component

---
+++ src/components/Link/styled.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export type StyledLinkProps = {}
+
+export const StyledLink = styled.a<StyledLinkProps>`
+  color: ${({ theme }) => theme.colors.lightGray};
+  text-decoration: none;
+
+  &:hover,
+  :focus {
+    text-decoration: underline;
+  }
+
+  padding: 0px ${({ theme }) => theme.spacing.md}px;
+
+  :not(:last-child) {
+    border-inline-end-width: 2px;
+    border-inline-end-style: solid;
+    border-inline-end-color: ${({ theme }) => theme.colors.lightGray};
+  }
+`
