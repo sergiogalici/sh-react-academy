df986a5fa6e002f16c77178216d35a4fb3341992
Alessio Ilardi <alessio.ilardi@gmail.com>
2023-02-06 15:53:47 +0100

    👌 IMPROVE: refactor Nav to expect an array of objects as prop

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,10 +1,23 @@
 import { ReactNode } from 'react'
+import { ThemeColors } from '../../style/theme'
+import { Link } from '../Link'
 import { StyledNav, StyledNavProps } from './styled'
 
-type Props = Partial<StyledNavProps> & {
-  children?: ReactNode
+export type NavLink = {
+  title: string
+  to: string
 }
 
-export const Nav = ({ children }: Props) => {
-  return <StyledNav>{children}</StyledNav>
+export type NavProps = Partial<StyledNavProps> & {
+  links: NavLink[]
+}
+
+export const Nav = ({ links }: NavProps) => {
+  return (
+    <StyledNav>
+      {links.map((link) => (
+        <Link to={link.to}>{link.title}</Link>
+      ))}
+    </StyledNav>
+  )
 }
