7724e33c24d19f58410e756439e8364c28e7a64b
sergiogalici <galici.sergio@gmail.com>
2023-02-08 19:00:31 +0100

    Small Product Container Style Changes

--- src/components/ProductsContainer/styled.ts
+++ src/components/ProductsContainer/styled.ts
@@ -5,9 +5,9 @@ export const StyledProdContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 35px;
-  background: rgb(233, 233, 233);
+  background: rgb(255, 255, 255);
   background: linear-gradient(
-    79deg,
+    90deg,
     rgba(233, 233, 233, 0.7) 0%,
     rgba(255, 255, 255, 1) 100%
   );
