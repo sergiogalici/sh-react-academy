cc26f77129fb979e81f7a81478eb4e671499574c
Alessio Ilardi <alessio.ilardi@gmail.com>
2023-02-14 16:33:23 +0100

    🐛 FIX: add optional chaining to ProductsContainer

--- src/components/ProductsContainer/index.tsx
+++ src/components/ProductsContainer/index.tsx
@@ -1,22 +1,21 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getAds, getCategories, getUsers } from '../../api'
-import { AdDto, CategoryDto, UserDto } from '../../api/type'
 import { adsActions } from '../../feature/ads/reducer'
 import { selectAllAds } from '../../feature/ads/selector'
 import { categoriesActions } from '../../feature/categories/reducer'
-import { getCategoryById, selectAllCategories } from '../../feature/categories/selectors'
+import { getCategoryById } from '../../feature/categories/selectors'
 import { usersActions } from '../../feature/users/reducer'
-import { getUsersById, selectAllUsers } from '../../feature/users/selector'
+import { getUsersById } from '../../feature/users/selector'
 import { ProductCard } from '../ProductCard'
 import { Text } from '../Text'
 import { StyledProdContainer } from './styled'
 
 export const ProductsContainer = () => {
   const dispatch = useDispatch()
-  const categoriesById = useSelector(getCategoryById)
   const usersById = useSelector(getUsersById)
   const ads = useSelector(selectAllAds)
+  const categoriesById = useSelector(getCategoryById)
 
   useEffect(() => {
     getAds()
@@ -32,32 +31,35 @@ export const ProductsContainer = () => {
       .catch((e) => console.log(e.message))
   }, [dispatch])
 
-  const mappedAds = ads.map(({ authorId, categoryIds, ...ad }) => {
-    return {
-      ...ad,
-      category: categoriesById[categoryIds[0]],
-      author: usersById[authorId]
-    }
-  })
+  const mappedAds =
+    ads &&
+    ads.map(({ authorId, categoryIds, ...ad }) => {
+      return {
+        ...ad,
+        category: categoriesById[categoryIds[0]],
+        author: usersById[authorId]
+      }
+    })
 
   return (
     <StyledProdContainer>
       <Text color="lightGray" variant="h6">{`${ads.length} risultati`}</Text>
       <Text variant="h6">Annunci</Text>
-      {mappedAds.map((ad) => {
-        return (
-          <ProductCard
-            authorName={ad.author.username}
-            category={ad.category.title}
-            rating={ad.author.rating}
-            title={ad.title}
-            price={ad.price}
-            description={ad.description}
-            imageSrc={ad.images[0]}
-            key={ad.id}
-          />
-        )
-      })}
+      {mappedAds &&
+        mappedAds.map((ad) => {
+          return (
+            <ProductCard
+              authorName={ad?.author?.username}
+              category={ad?.category?.title}
+              rating={ad?.author?.rating}
+              title={ad?.title}
+              price={ad?.price}
+              description={ad?.description}
+              imageSrc={ad?.images[0]}
+              key={ad?.id}
+            />
+          )
+        })}
     </StyledProdContainer>
   )
 }
